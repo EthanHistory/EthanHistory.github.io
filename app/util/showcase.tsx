8c07ec17a21df2552a8134ed470619bec10570d4
EthanHistory <hanpersonality@gmail.com>
2024-08-25 01:32:01 -0700

    Basic structure

--- app/util/showcase.tsx
+++
@@ -1,7 +0,0 @@
-const showCase = {
-    "Resume" : "User wants to read the pdf file of Inseong's resume",
-    "Projects" : "User wants to ",
-    "Education" : "",
-    "Work Exp" : "",
-    "Tech Skills" : "",
-};
